ee1edf5f8827cbe3b274be2ef4898fe332886d92
Tamar Gefner <tgwork640@gmail.com>
2024-03-26 13:01:50 +0200

    movie change

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,6 +9,6 @@ namespace WebApplication2.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
-        public decimal Price { get; set; }
+        public decimal PriceD { get; set; }
     }
 }
