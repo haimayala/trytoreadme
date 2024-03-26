173099d321179dd98a9f74ef8af934108737b127
AYALA HAIM <ay4117096@gmail.com>
2024-03-26 12:52:33 +0200

    first push

---
+++ Data/WebApplication2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class WebApplication2Context : DbContext
+    {
+        public WebApplication2Context (DbContextOptions<WebApplication2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication2.Models.Movie> Movie { get; set; } = default!;
+
+        public DbSet<WebApplication2.Models.student>? student { get; set; }
+    }
+}
